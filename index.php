d684ce4b1dbeaa6febd5262b871081b8dbd268ec
ManguiEliezer <eliezermangui22@gmail.com>
2023-05-31 20:36:29 +0800

    button function added

--- index.php
+++ index.php
@@ -91,10 +91,11 @@ if(isset($_REQUEST['login_button']) === true){
 		      				}
 
 		      			?>
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -108,7 +109,7 @@ if(isset($_REQUEST['login_button']) === true){
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
@@ -117,11 +118,5 @@ if(isset($_REQUEST['login_button']) === true){
 		</div>
 	</section>
 
-  <!--<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script> -->
-
 	</body>
 </html>
-
